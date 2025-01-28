2b3e9386d782f64844606ba99ec5b351c4178d0c
sunny <sj0041@naver.com>
2025-01-28 15:02:39 +0900

    update

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   const [name] = useState(["구라댕씨를", "몰아내자", "타도구라댕"]);
 
   return (
-    <div>
+    <div className="wrapper">
+      <Header />
       {name.map(function (item) {
         return <div className="box">{item}</div>;
       })}
---
+++ my-app/src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <h1 className="header__logo">logo</h1>
+      <h2 className="header__title">타이틀</h2>
+      <a href="#">햄버거메뉴</a>
+    </header>
+  );
+}
