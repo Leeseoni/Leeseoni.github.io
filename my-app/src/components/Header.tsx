867af4845e7938849f5c4f30227c2db6a215c435
sunny <kkuk79@naver.com>
2025-02-12 17:21:00 +0900

    mm

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,11 +1,14 @@
-export default function Header() {
+import styles from "../assets/styles/components/_Header.module.scss";
+
+const Header = () => {
   return (
-    <header>
-      <h1 className="header__logo">
-        <div className="header__subtitle">타이틀</div>
-      </h1>
-      <h2 className="header__title">타이틀</h2>
-      <div className="header__btn--menu">햄버거메뉴</div>
+    <header className={styles.header}>
+      {/* <h1 className={styles.header__logo}>logo</h1> */}
+      <div className={styles.header__btn__back}>뒤로가기</div>
+      <h2 className={styles.header__title}>서울센트럴파크케슬</h2>
+      <div className={styles.header__btn__menu}>메뉴</div>
     </header>
   );
-}
+};
+
+export default Header;
